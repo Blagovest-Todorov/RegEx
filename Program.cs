a504c3c5b2092fb7f081a33b36212f24f3193416
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-19 08:57:34 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Today is 2015-05-11";
+            string pattern = @"\d{4}-\d{2}-\d{2}";
+
+            Regex regex = new Regex(pattern);
+            bool containsValidDate = regex.IsMatch(text);
+
+            Console.WriteLine(containsValidDate);
+
+        }
+    }
+}
